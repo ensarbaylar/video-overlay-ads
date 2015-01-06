cbf058be8ebebadf4b86968c0cb092f4ecfdb266
ensarbaylar <ensarbaylar@yahoo.com.tr>
2015-01-06 22:31:44 +0200

    go

---
+++ main.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Wordpress Video Overlay Ads
+Version: 1.0.0
+Plugin URI: http://ensarbaylar.com/
+Author: ensar
+Author URI: http://ensarbaylar.com/
+Description: Add overlay advertisement area over youtube video embeds before video starts
+*/
+
+?>
