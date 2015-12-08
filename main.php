7c9ccc2e9b044cc234ac5682cb63bf3707100b85
ensarbaylar <ensarbaylar@yahoo.com.tr>
2015-12-08 17:43:11 +0200

    151208-1

--- main.php
+++ main.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: WP Video Overlay Ads
 Version: 1.0.0
-Plugin URI: http://ensarbaylar.com/
-Author: ensar
-Author URI: http://ensarbaylar.com/
+Plugin URI: http://salinus.com/
+Author: Salinus
+Author URI: http://salinus.com/
 Description: Add overlay advertisement area over youtube video embeds before video starts
 */
 
